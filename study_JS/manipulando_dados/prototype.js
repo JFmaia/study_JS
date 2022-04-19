2f8df4f1a5fad8a70fb2b5c23adbb487a62e42b4
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-19 20:13:18 -0300

    Nova pasta sobre manipulação de dados.
    Nesta pasta será guardado todo assunto sobre manipulação de dados, começando já com o arquivo sobre o prototype do JS.

---
+++ study_JS/manipulando_dados/prototype.js
@@ -0,0 +1,7 @@
+/*
+O JavaScript possui uma característica chamada Prototype, que é uma série de funcionalidades e atributos que já existem nativamente em diversos tipos de dados, que são lidados como objetos.
+ */
+
+// Podemos observar que o Prototype é um objeto, e que ele possui métodos e atributos.
+// Aqui no array já um metodo length, que retorna o tamanho do array.
+let list = [1, 2, 3, 4, 5].length;
