de041a8d31cc43bb0e39a9bc021dfdcab7686a62
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-19 20:20:12 -0300

    Arquivo sobre type conversion e type coersion.

---
+++ study_JS/manipulando_dados/type_conversion.js
@@ -0,0 +1,12 @@
+/* 
+    Type conversion (type casting) vs Type Coersion
+    * Alterações de um tipo de dado para outro.
+*/
+
+// Exemplo:
+
+// Type coersion
+console.log('9'+ 5); // exibe 95 transformando tudo automaticamente em string.
+
+// Type conversion
+console.log(Number('9') + 5); // exibe 14, pois o operador + é do tipo number.
