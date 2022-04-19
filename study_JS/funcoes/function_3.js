91a241227ec89adb9dc5e86a1b2a97985b30d59a
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-19 14:24:38 -0300

    Terceiro execício sobre funções.
    Para praticar!

---
+++ study_JS/funcoes/function_3.js
@@ -0,0 +1,14 @@
+// function expression
+// function anonymous
+
+// parâmetros de uma função
+const sum = function(number1, number2){
+    // se não houver retorno, retorna undefined
+    return number1 + number2
+}
+
+let number1 = 34;
+let number2 = 56;
+
+console.log(`A soma é ${sum(number1, number2)}`); // Argumentos
+ 
