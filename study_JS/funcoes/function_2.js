7f2a848cf6185c65b3e56e123dcdf47bfb7c44fe
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-19 14:24:20 -0300

    Segundo execício sobre funções.
    Para praticar!

---
+++ study_JS/funcoes/function_2.js
@@ -0,0 +1,10 @@
+// function expression
+// function anonymous
+
+// parâmetros de uma função
+const sum = function(number1, number2){
+    console.log(number1 + number2);
+}
+
+sum(1,2); // Argumentos
+
