a07af0f6cf1d92d53dbdd2c1b4d8417e2035172e
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:35:06 -0300

    Arquivo sobre typeof e delete.

---
+++ study_JS/expressions_operetaions/typeof_delete.js
@@ -0,0 +1,24 @@
+/* Operadores unários
+
+    typeof
+    delete
+
+*/
+//typeof
+console.log(typeof "José");
+
+//delete
+const person ={
+    //propriedades
+    nome: 'Jose',
+    age: 22,
+}
+
+delete person.age;
+
+
+
+
+
+
+
