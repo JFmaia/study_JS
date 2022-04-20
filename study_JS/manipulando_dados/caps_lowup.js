26fe793695ce17c6a3eb437a8d90a2f10464b61a
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 08:45:50 -0300

    Arquivo sobre letras maiusculas e minusculas..

---
+++ study_JS/manipulando_dados/caps_lowup.js
@@ -0,0 +1,8 @@
+// Manipulando String e Números
+
+// Transformando letras minúsculas em maiúsculas. Faça o contrário disso também.
+
+let word = "Programar é muito bacana!";
+console.log(word.toUpperCase());
+//ou
+console.log(word.toLowerCase());
