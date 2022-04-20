9ddba88830264f9a8633ad087bdae3a4cce0d3c4
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:36:19 -0300

    Arquivo sobre operadores de comparação de strings.

---
+++ study_JS/expressions_operetaions/operadores_string.js
@@ -0,0 +1,17 @@
+/* Operadores de String*/
+
+// comparison
+console.log("a"=="b");
+
+// concatenação
+// Retorna a união de duas strings
+console.log("a"+"b"+"c");
+// ou
+let alpha = "alpha";
+alpha += "bet";
+// mostrará "alphabet" atulalizando a variável
+console.log(alpha);
+
+
+
+
