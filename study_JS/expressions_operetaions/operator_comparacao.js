8c6d13e4c6d2720105c37d7e0e1019c4f6582e34
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:35:54 -0300

    Arquivo sobre operadores de comparação.

---
+++ study_JS/expressions_operetaions/operator_comparacao.js
@@ -0,0 +1,17 @@
+/* Operador de comparação */
+
+// Irá comparar valores e retornar um Boolean como reposta à comparação
+
+// = atribuindo valor
+let one =1;
+let two =2;
+
+// == igual a
+console.log(one == two);
+// != diferente de
+console.log(one != two);
+
+
+
+
+
