e020e21eb0332cfae268529dc628e2a0ea59154d
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:38:56 -0300

    Arquivo sobre grouping ().

---
+++ study_JS/expressions_operetaions/grouping.js
@@ -0,0 +1,9 @@
+/* Grouping operators ( ) */
+
+// let total = 2 + 2 * 5; // 12
+// Com o uso de parênteses, o cálculo é feito primeiro os parênteses e depois os operadores.
+let total = (2 + 2) * 5; // 20
+console.log(total); 
+
+
+
