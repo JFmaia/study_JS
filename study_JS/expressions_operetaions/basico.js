c405c5df23fea68daaede78ace7b31d4b362de1a
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:40:14 -0300

    Arquivo sobre operadores basicos.

---
+++ study_JS/expressions_operetaions/basico.js
@@ -0,0 +1,22 @@
+/* Expressões e Operadores
+
+- Expressions
+- Operators
+    Binary
+    Unary
+    Ternary
+*/
+
+let number = 1;
+
+// somando mais um ao número com dois operadores binarios '++' ou '--'.
+console.log(++number);
+
+// Operador unario
+console.log(typeof number);
+
+//Ternario unico caso
+console.log(number > 0 ? 'Positivo' : 'Negativo');
+
+
+
