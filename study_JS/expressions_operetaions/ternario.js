31c98c8c46437fa0945ffab22009d6291622a35c
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:35:25 -0300

    Arquivo sobre operadores ternarios.

---
+++ study_JS/expressions_operetaions/ternario.js
@@ -0,0 +1,19 @@
+/* Operadores condicionais (Ternario)*/
+
+// Dependendo da condição, nós receberemos valores diferentes
+
+// Condição então valor 1 se não valor 2
+// condition ? value1 : value2
+
+// Exemplos
+
+// Café da manhã bom
+let pao = true;
+let carne = true;
+const niceBreakfast = pao && carne ? "Perfeito!" : "Errado meu café da manhão!";
+
+// Maior que 18 anos
+let age = 18;
+const ageApropiety = age >= 18 ? "Maior de idade" : "Menor de idade";
+
+
