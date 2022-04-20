c7eabe7828439dd67fe6dad21dc5551c9734bdd8
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:38:26 -0300

    Arquivo sobre operadores logicos.

---
+++ study_JS/expressions_operetaions/logico.js
@@ -0,0 +1,24 @@
+/* Operadores logicos */
+
+// - 2 valores booleanos, quando verificados, resultará em verdadeiro ou falso
+
+let pao = true
+let carne = true
+
+// AND &&
+console.log(pao && carne);
+
+// OR ||
+console.log(pao || carne);
+
+// NOT !
+console.log(!pao);
+
+/* Operadores de atribuição (Assignment) */
+
+let x;
+
+console.log(x);
+
+
+
