c4ffec4417a9b6eebe47e2a7bfa01eabb9d01e23
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 08:49:27 -0300

    Arquivo sobre verificação de palavras.
    Verificando se uma palavra estar contida em uma frase ou não.

---
+++ study_JS/manipulando_dados/verif_palavra.js
@@ -0,0 +1,8 @@
+// Manipulando Strings e Array
+
+// Verificando se o texto contém a palavra "Amor"
+
+let pharase = "Amor é a palavra de mais bonita!";
+// Verificando se a palavra "Amor" está contida no texto
+console.log(pharase.includes("Amor"));
+// O include é sensivel a caixa alta e caixa baixa
