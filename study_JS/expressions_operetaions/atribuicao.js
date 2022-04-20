f455c386bc88848a7bf6ad6989568d12bc27f1c8
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:40:33 -0300

    Arquivo sobre operadores de atribuição.

---
+++ study_JS/expressions_operetaions/atribuicao.js
@@ -0,0 +1,21 @@
+/* Operadores de atribuição */
+
+// assignment 
+x = 1;
+
+// addintion assingment
+x += 1;
+
+// substraction assingment
+x -= 1;
+
+// multiplication assingment
+x *= 1;
+
+// division assingment
+x /= 1;
+
+
+
+
+
