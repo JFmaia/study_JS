6281eedb19626f1002c10398f0ed88c0a1aefe04
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:37:36 -0300

    Arquivo sobre maior > e menor < que.

---
+++ study_JS/expressions_operetaions/operador_maior_menor.js
@@ -0,0 +1,20 @@
+/* Operadores estritamente */
+
+// = atribuindo valor
+let one =1;
+let two =2;
+
+// > Maior que
+console.log(one > two);
+
+// >= Maior igual a
+console.log(one >= two);
+
+// < Menor que
+console.log(one < two);
+
+// <= Menor igual a
+console.log(one <= two);
+
+
+
