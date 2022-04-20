3e49e04868ce5efae6c879329dbd7a41084383c9
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:39:54 -0300

    Arquivo sobre operadores estrimententes.

---
+++ study_JS/expressions_operetaions/estritamente.js
@@ -0,0 +1,16 @@
+/* Operadores estritamente */
+
+// = atribuindo valor
+let one =1;
+let two =2;
+
+// === estritamente igual a
+// comparação de tipos e valor
+console.log(one === two);
+
+// !== estritamente diferente de
+// diferença de tipos e valor
+console.log(one !== two);
+
+
+
