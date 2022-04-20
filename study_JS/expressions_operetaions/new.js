122f0f8aca4f10dca782f19e885d414a9c13177b
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:38:03 -0300

    Arquivo sobre a expressão 'new'.

---
+++ study_JS/expressions_operetaions/new.js
@@ -0,0 +1,19 @@
+/* Expressão
+
+    new 
+
+    * left-hand-side expression
+    * cria um novo objeto
+
+*/
+
+let nome = new String('João');
+// adicionando um novo atributo ao objeto
+nome.surName = 'Silva';
+let age = new Number(20);
+console.log(nome, age);
+
+
+
+
+
