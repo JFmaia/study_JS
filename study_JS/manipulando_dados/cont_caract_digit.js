367fe5ecc38c521f4ad1a992c9e180a08cbdee8a
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 08:46:45 -0300

    Arquivo sobre contagem de caracteres e digitos.

---
+++ study_JS/manipulando_dados/cont_caract_digit.js
@@ -0,0 +1,9 @@
+// Manipulando String e Números
+
+// Contar quantos caracteres tem uma palavrá e quantos dígitos tem um número
+
+let word ="MamãoPapaia";
+console.log(word.length);
+let tel = 888552211;
+// Outra maneira de transformar um número em string
+console.log(tel.toString().length);
