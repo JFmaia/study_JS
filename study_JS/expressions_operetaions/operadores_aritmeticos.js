5dad0c16f92d8708ab96ff04bd53c500233088de
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 20:37:01 -0300

    Arquivo sobre operações aritméticas.

---
+++ study_JS/expressions_operetaions/operadores_aritmeticos.js
@@ -0,0 +1,21 @@
+/* Operadores aritméticos*/
+
+// multiplicação
+console.log(2 * 2);
+// divisão
+console.log(2 / 2);
+// soma
+console.log(2 + 2);
+// subtração
+console.log(2 - 2);
+// resto de divisão
+console.log(2 % 2);
+// incremento
+console.log(2++);
+// decremento
+console.log(2--);
+// exponencial
+console.log(2 ** 2);
+
+
+
