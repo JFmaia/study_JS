2dfb9a92df522903756d50118211dab9d98f2f62
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-20 08:48:27 -0300

    Arquivo sobre transformação de string para numero e numero para string.

---
+++ study_JS/manipulando_dados/string_number.js
@@ -0,0 +1,7 @@
+// Manipulando String e Números
+
+// Transformar String em Número e Número em String
+let string = "10";
+console.log(Number(string));
+let number = 10;
+console.log(String(number));
