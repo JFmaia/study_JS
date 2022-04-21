df4426aea880cf20bc037200a5b236690594c8cc
José Flávio S. Maia <jfmaia741@gmail.com>
2022-04-21 09:32:42 -0300

    Arquivo sobre for of.

---
+++ study_JS/struct_repetion/for_of.js
@@ -0,0 +1,14 @@
+// Estrutura de repetição
+
+// for...of
+
+let nome = 'José';
+let names = ['José', 'Maria', 'João'];
+
+// muito bom para listar itens de array ou list
+for(let elements of names){
+    console.log(elements);
+}
+
+
+
